38f6fdc9dd6d5b0f2bc91b5d8ee26a274bf80462
Eross Thanatos <erossthanatos@gmail.com>
2022-09-26 21:04:10 +0330

    first commit

---
+++ scripts.js
@@ -0,0 +1,5 @@
+let weight = Number(prompt("Enter Your Weight(kg)"));
+let height = Number(prompt("Enter Your Height(m)"));
+let bmiIndex = weight/(height*height);
+console.log(bmiIndex);
+alert("your BMI is : " + bmiIndex);
