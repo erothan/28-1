4b7ed30396c0a4a59fc8fbd7f4d5b9a63cde6678
Eross Thanatos <erossthanatos@gmail.com>
2022-09-28 17:05:15 +0330

    add qualitative description

--- scripts.js
+++ scripts.js
@@ -1,5 +1,17 @@
 let weight = Number(prompt("Enter Your Weight(kg)"));
 let height = Number(prompt("Enter Your Height(m)"));
-let bmiIndex = weight/(height*height);
-console.log(bmiIndex);
-alert("your BMI is : " + bmiIndex);
+let bmi = weight / (height * height);
+
+console.log(bmi);
+
+if (bmi >= 18.5 && bmi <= 25) {
+    alert('your BMI is ' + bmi + '   Your bmi is normal.');
+}
+
+if (bmi > 25) {
+    alert('your BMI is ' + bmi  + '  Your bmi is overweight.');
+}
+
+if (bmi < 18.5) { 
+    alert('your BMI is ' +  bmi  + '  Your bmi is underweight.');
+}
